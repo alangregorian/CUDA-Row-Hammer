08a5b08bb621d9c9d7acd0c7e47a08f87d0e080b
agregorian3 <agregorian3@gatech.edu>
2024-06-26 12:36:43 -0500

    copying over

--- tests/simple_write_latency.cu
+++ tests/simple_write_latency.cu
@@ -1,17 +1,20 @@
 #include <cuda_runtime.h>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 #define N 1024*1024  // Number of iterations
 
-__global__ void write_latency_test(volatile int* d_array, unsigned long long* d_time, int value) {
-    for (int idx = 0; idx < 1000; idx++) {
-        unsigned long long start_time = clock64();
+__global__ void write_latency_test(volatile int* d_array, const int* d_random_values, unsigned long long* d_time) {
+    for (int idx = 0; idx < 250; idx++) {
+        unsigned long long start_time = clock();
 
+        int temp = d_random_values[idx];
         // Write operation
-        d_array[idx] = value;
+        d_array[idx] = temp;
 
-        unsigned long long end_time = clock64();
+        unsigned long long end_time = clock();
 
         // Record the latency
         d_time[idx] = end_time - start_time;
@@ -19,15 +22,25 @@ __global__ void write_latency_test(volatile int* d_array, unsigned long long* d_
 }
 
 int main() {
+    srand(time(0));
+
     volatile int *d_array;
+    int *d_random_values;
     unsigned long long *d_time;
     cudaMalloc((void**)&d_array, N * sizeof(volatile int));
+    cudaMalloc(&d_random_values, N * sizeof(int));
     cudaMalloc(&d_time, N * sizeof(unsigned long long));
 
-    int value = 42;
+    int *h_random_values = new int[N];
+    for (int i = 0; i < N; i++) {
+        h_random_values[i] = rand() % 1000;  // Random values between 0 and 999
+    }
+
+    // Copy random values to device
+    cudaMemcpy(d_random_values, h_random_values, N * sizeof(int), cudaMemcpyHostToDevice);
 
     // Launch a single thread
-    write_latency_test<<<1, 1>>>(d_array, d_time, value);
+    write_latency_test<<<1, 1>>>(d_array, d_random_values, d_time);
     cudaDeviceSynchronize();
 
     // Copy the timing data back to the host
@@ -37,13 +50,15 @@ int main() {
     // Save the timings to a CSV file
     std::ofstream csv_file("write_latencies.csv");
     csv_file << "Iteration,Time\n";
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 250; i++) {
         csv_file << i << "," << h_time[i] << "\n";
     }
     csv_file.close();
 
+    delete[] h_random_values;
     delete[] h_time;
     cudaFree((void*)d_array);
+    cudaFree(d_random_values);
     cudaFree(d_time);
     return 0;
 }
